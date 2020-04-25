e6f96f9154e6ab64bf79d95e577f0b4284858d3d
QArtur99 <artur2113@o2.pl>
2020-04-26 01:56:49 +0200

    Fix: menu size

--- app/src/main/java/com/artf/shopinglistcompose/ui/view/menu/MainMenu.kt
+++ app/src/main/java/com/artf/shopinglistcompose/ui/view/menu/MainMenu.kt
@@ -1,7 +1,6 @@
 package com.artf.shopinglistcompose.ui.view.menu
 
 import androidx.compose.Composable
-import androidx.compose.MutableState
 import androidx.compose.state
 import androidx.ui.core.*
 import androidx.ui.foundation.Icon
@@ -14,21 +13,23 @@ import androidx.ui.material.Surface
 import androidx.ui.material.TextButton
 import androidx.ui.res.stringResource
 import androidx.ui.res.vectorResource
+import androidx.ui.unit.Dp
 import androidx.ui.unit.IntPx
 import androidx.ui.unit.IntPxPosition
 import androidx.ui.unit.dp
 import com.artf.shopinglistcompose.R
 import com.artf.shopinglistcompose.ui.view.SharedViewModel
 import com.artf.shopinglistcompose.ui.view.layout.Screen
-import com.artf.shopinglistcompose.ui.view.layout.ScreenAmbient
+import com.artf.shopinglistcompose.ui.view.layout.ScreenBackStackAmbient
 import com.artf.shopinglistcompose.util.dpToPx
+import com.artf.shopinglistcompose.util.pxToDp
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @Composable
 fun MainMenu() {
     val sharedViewModel = LifecycleOwnerAmbient.current.viewModel<SharedViewModel>()
     val showMenu = state { false }
-    val backStack = ScreenAmbient.current
+    val backStack = ScreenBackStackAmbient.current
 
     IconButton(onClick = { showMenu.value = true }) {
         Icon(vectorResource(R.drawable.ic_baseline_more_vert_black_24))
@@ -40,7 +41,7 @@ fun MainMenu() {
     ) {
         Row(Modifier.padding(16.dp, 16.dp, 4.dp, 16.dp)) {
             Surface(
-                Modifier.width((ConfigurationAmbient.current.screenWidthDp * 0.6).dp),
+                Modifier.width((getMenuSize())),
                 shape = RoundedCornerShape(4.dp),
                 elevation = 8.dp
             ) {
@@ -71,4 +72,15 @@ fun MainMenu() {
             }
         }
     }
+}
+
+@Composable
+fun getMenuSize(): Dp {
+    val spaceForChar = 16
+    var result = 0
+    mutableListOf<String>().apply {
+        add(stringResource(id = R.string.current_shopping_list))
+        add(stringResource(id = R.string.archived_shopping_list))
+    }.map { if (it.length > result) result = it.length }
+    return ((result * spaceForChar).pxToDp() + 24 + 12).dp
 }
