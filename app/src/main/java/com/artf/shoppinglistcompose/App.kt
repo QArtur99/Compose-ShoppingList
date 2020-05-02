59cb2dc61b175d828160a0349380cdef1d8d5aa6
QArtur99 <artur2113@o2.pl>
2020-05-02 03:14:40 +0200

    Refactor: di

--- app/src/main/java/com/artf/shoppinglistcompose/App.kt
+++ app/src/main/java/com/artf/shoppinglistcompose/App.kt
@@ -3,7 +3,6 @@ package com.artf.shoppinglistcompose
 import android.app.Application
 import com.artf.shoppinglistcompose.di.dataModule
 import com.artf.shoppinglistcompose.di.uiDataModule
-import com.artf.shoppinglistcompose.di.vmModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -19,7 +18,7 @@ class App : Application() {
         startKoin {
             androidLogger()
             androidContext(this@App)
-            modules(listOf(dataModule, uiDataModule, vmModule))
+            modules(listOf(dataModule, uiDataModule))
         }
     }
 }
